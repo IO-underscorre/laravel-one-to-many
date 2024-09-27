2bb17aefc085ad448e4c1c473407e61ea1517338
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-09-27 02:43:21 +0200

    fix(posts): table seeder edited to include the post types table foreign key

--- database/seeders/PostsTableSeeder.php
+++ database/seeders/PostsTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Functions\Helper;
 use Illuminate\Database\Seeder;
 use App\Models\Post;
+use App\Models\PostType;
 use Faker\Generator as Faker;
 
 class PostsTableSeeder extends Seeder
@@ -23,6 +24,8 @@ class PostsTableSeeder extends Seeder
             $new_post->body = $faker->paragraph(8);
             $new_post->reading_time = Helper::getReadingTime($new_post->body);
             $new_post->is_archived = boolval(rand(0, 1));
+
+            $new_post->post_type_id = rand(0, 20) ? PostType::inRandomOrder()->first()->id : null;
 
             $new_post->save();
         }
