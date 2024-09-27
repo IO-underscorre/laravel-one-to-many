77dc8c9278a84c08e62d8707a7a4bc221aa134e7
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-09-27 06:31:06 +0200

    fix(posts): CRUD's index table displays better data

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -13,32 +13,32 @@
 
             <ul class="list-group list-group-flush d-flex">
                 <li class="list-group-item overflow-auto p-0">
-                    <table class="table table-striped table-hover align-middle mb-0">
+                    <table class="table table-striped table-hover align-middle mb-0 text-center">
                         <thead>
                             <tr>
-                                <th scope="col" class="text-primary col-1">#</th>
+                                <th scope="col" class="text-primary text-start col-1">#</th>
 
-                                <th scope="col" class="text-primary col-2">Title</th>
+                                <th scope="col" class="text-primary col-4">Title</th>
 
-                                <th scope="col" class="text-primary col-5">Body</th>
+                                <th scope="col" class="text-primary col-3">Type</th>
 
-                                <th scope="col" class="text-primary text-center col-1">Archived</th>
+                                <th scope="col" class="text-primary col-1">Status</th>
 
-                                <th scope="col" class="text-primary text-center col-3">Options</th>
+                                <th scope="col" class="text-primary col-3">Options</th>
                             </tr>
                         </thead>
                         <tbody>
                             @foreach ($posts as $post)
                                 <tr>
-                                    <th scope="row" class="col-1">{{ $post['id'] }}</th>
+                                    <th scope="row" class="col-1 text-start">{{ $post['id'] }}</th>
 
-                                    <td class="col-2">{{ $post['title'] }}</td>
+                                    <td class="col-4 text-start">{{ $post['title'] }}</td>
 
-                                    <td class="col-5">{{ $post['body'] }}</td>
+                                    <td class="col-3">{{ isset($post->postType) ? $post->postType->name : 'None' }}</td>
 
-                                    <td class="text-center col-1">{{ $post['is_archived'] ? 'Yes' : 'No' }}</td>
+                                    <td class="col-1">{{ $post['is_archived'] ? 'Archived' : 'Active' }}</td>
 
-                                    <td class="text-center col-3">
+                                    <td class="col-3">
                                         <menu class="d-flex justify-content-center gap-1">
                                             <a href="{{ route('admin.posts.show', $post) }}" class="btn btn-sm btn-primary">
                                                 <i class="fa-solid fa-eye"></i>
