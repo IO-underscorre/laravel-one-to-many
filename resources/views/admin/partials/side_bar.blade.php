50a0bf474915cf087deb32b2b7a1992dcf74e416
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-09-28 05:41:50 +0200

    feat: side bar links now display when the current page is the index or any other relative CRUD's page

--- resources/views/admin/partials/side_bar.blade.php
+++ resources/views/admin/partials/side_bar.blade.php
@@ -7,7 +7,8 @@
         @foreach ($side_bar_links as $link)
             <li>
                 <a href="{{ route($link['route_name']) }}"
-                    class="d-flex gap-2 nav-link position-relative {!! Route::currentRouteName() === $link['route_name'] ? 'active' : null !!}">
+                    class="d-flex gap-2 nav-link position-relative
+                    @if (explode('.', Route::currentRouteName())[1] == explode('.', $link['route_name'])[1]) active @endif">
                     <div class="flex-shrink-0">
                         {!! $link['menu_icon'] !!}
                     </div>
